ebe2612de3e0a37863754833583b42d0ddb7373d
Daniel Branco <daab.daab@gmail.com>
2022-01-08 20:10:05 +0100

    Developing of the Rover class
    Defined the state it will maintain:
     - int[] roverFacedDirection;
     - int[][] roverPositionOnTheGrid.
    
    Named the first methods that will define his behavior when commanded to move:
    
     - void rotateRover;
     - boolean moveRover;
     - private int[] getRoverPosition;
     - private String getRoverFacedDirection.
    
    Implemented getRoverFacedDirection to read the roverFacedDirection
    and return a String with the direction is facing.

--- Simple Mars Rover/src/learning/branco/daniel/CoduranceKatas/SimpleMarsRover/Rover.java
+++ Simple Mars Rover/src/learning/branco/daniel/CoduranceKatas/SimpleMarsRover/Rover.java
@@ -1,4 +1,57 @@
 package learning.branco.daniel.CoduranceKatas.SimpleMarsRover;
 
-public class Rover {
+class Rover {
+
+    /*Array that keeps a record of the direction that the rover is facing.
+    1 represents the faced direction, 0 the other direction {North, East, South, West}*/
+    private int[] roverFacedDirection = {1,0,0,0};
+
+    /*Array that shows the rover position on the grid 10x10
+    1 represents his position, 0 the available positions
+    The first sub-array is the x-axis of the grid where the index 0 represents the west
+    and the index 9 represents the East
+    The second sub-array is the y-axis of the grid where the index 0 represents the South
+    and the index 9 represents the North
+     */
+    private int[][] roverPositionOnTheGrid = {{1,0,0,0,0,0,0,0,0,0},{1,0,0,0,0,0,0,0,0,0}};
+
+    //Method that rotates the rover, can receive two directions Left or Right
+    void rotateRover(String direction){
+
+    }
+
+    /*Method that moves the rover
+    if it reaches the end of grid it must ignore the move commands
+    until a new valid direction is supplied
+    */
+    boolean moveRover(){
+
+    }
+
+    //Returns a int Array with the rover position on the grid
+    private int[] getRoverPosition(){
+
+    }
+
+    //Returns the direction the rover is facing
+    private String getRoverFacedDirection(){
+        int indexDirectionFaced = 0;
+        for(int i = 0; i < 4; i++){
+            if(roverFacedDirection[i] == 1){
+                indexDirectionFaced = i;
+                break;
+            }
+        }
+
+        switch (indexDirectionFaced){
+            case 0:
+                return "north";
+            case 1:
+                return "East";
+            case 2:
+                return "South";
+            case 3:
+                return "West";
+        }
+    }
 }
