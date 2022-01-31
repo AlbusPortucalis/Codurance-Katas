13e574a212f508d4a071b9a8a1872d71047935c5
Daniel Branco <daab.daab@gmail.com>
2022-01-31 10:10:54 +0100

    Create first classes and test to solve FizzBuzz kata

---
+++ FizzBuzz/src/learning/branco/daniel/CoduranceKatas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+package learning.branco.daniel.CoduranceKatas.FizzBuzz;
+
+public class FizzBuzz {
+
+    public String convert(int number){
+
+        throw new UnsupportedOperationException("implement me!");
+    }
+}
