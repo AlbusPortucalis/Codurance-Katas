0210993b68ab7c4827ed8a15366ca2e8bc62f8cf
Daniel Branco <daab.daab@gmail.com>
2022-02-03 11:55:15 +0100

    Added more tests to confirm that the code works with all cases

--- LeapYear/src/learning/branco/daniel/CoduranceKatas/leapYear/LeapYearShould.java
+++ LeapYear/src/learning/branco/daniel/CoduranceKatas/leapYear/LeapYearShould.java
@@ -8,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeapYearShould {
 
     @ParameterizedTest
-    @CsvSource({"1997, false", "1996, true", "1600, true", "1800, false"})
+    @CsvSource({"1997, false", "1996, true", "1600, true", "1800, false", "1700, false", "1900, false"
+                , "2100, false", "2000, true", "2004, true", "2020, true", "2001, false", "2010, false"})
     void is_year_leap(int inputYear, boolean expectedOutput){
         assertEquals(expectedOutput, new LeapYear().isLeapYear(inputYear));
     }
