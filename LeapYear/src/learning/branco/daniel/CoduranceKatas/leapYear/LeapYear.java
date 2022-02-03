432b6b04398c191c48c158c88daa382f26a4b1bf
Daniel Branco <daab.daab@gmail.com>
2022-02-03 11:49:34 +0100

    Added 4th test added code that inforces the last two rules
    Test -> 1800 not leap year
    Code -> had to apply the last two rules:
     - Is leap if divisible by 400
     - Is not leap if divisible by 100 but not 400

--- LeapYear/src/learning/branco/daniel/CoduranceKatas/leapYear/LeapYear.java
+++ LeapYear/src/learning/branco/daniel/CoduranceKatas/leapYear/LeapYear.java
@@ -3,10 +3,10 @@ package learning.branco.daniel.CoduranceKatas.leapYear;
 public class LeapYear {
 
     public boolean isLeapYear(int year){
-        boolean isLeap = false;
+        boolean isLeap = true;
 
-        if(year % 4 == 0){
-            isLeap = true;
+        if(year % 4 != 0 || (year % 100 == 0 && year % 400 != 0)){
+            isLeap = false;
         }
         return isLeap;
     }
--- LeapYear/src/learning/branco/daniel/CoduranceKatas/leapYear/LeapYearShould.java
+++ LeapYear/src/learning/branco/daniel/CoduranceKatas/leapYear/LeapYearShould.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LeapYearShould {
 
     @ParameterizedTest
-    @CsvSource({"1997, false", "1996, true", "1600, true"})
+    @CsvSource({"1997, false", "1996, true", "1600, true", "1800, false"})
     void is_year_leap(int inputYear, boolean expectedOutput){
         assertEquals(expectedOutput, new LeapYear().isLeapYear(inputYear));
     }
