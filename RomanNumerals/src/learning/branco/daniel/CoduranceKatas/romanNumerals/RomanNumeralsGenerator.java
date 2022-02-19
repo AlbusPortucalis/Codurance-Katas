97eb454fb223ca2d03b94799c2d415642b3debfb
Daniel Branco <daab.daab@gmail.com>
2022-02-19 15:40:51 +0100

    Added test 4th roman numeral and made it green

--- RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
+++ RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
@@ -2,11 +2,14 @@ package learning.branco.daniel.CoduranceKatas.romanNumerals;
 
 public class RomanNumeralsGenerator {
     private String unitsRomanNumeral = "I";
+    private String fourthRomanNumeral = "IV";
     public String convert(int amount){
         if(amount == 2){
             return unitsRomanNumeral.repeat(2);
         } else if (amount == 3){
             return unitsRomanNumeral.repeat(3);
+        } else if(amount == 4){
+            return fourthRomanNumeral;
         }
         return unitsRomanNumeral;
     }
