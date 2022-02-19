edde557c99a808f63b07943e4137ed79abd7175f
Daniel Branco <daab.daab@gmail.com>
2022-02-19 12:48:51 +0100

    1st test green1st test green1st test green1st test green1st test green1st test green1st test green1st test green1st test green1st test green1st test green

--- RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
+++ RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
@@ -3,6 +3,6 @@ package learning.branco.daniel.CoduranceKatas.romanNumerals;
 public class RomanNumeralsGenerator {
     public String convert(int amount){
 
-        return null;
+        return "I";
     }
 }
