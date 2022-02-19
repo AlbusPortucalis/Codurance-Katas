01d1203dce1b8eacafd071939511f7489bf82950
Daniel Branco <daab.daab@gmail.com>
2022-02-19 13:37:02 +0100

    Refactored test cases to reduce duplication

--- RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGeneratorShould.java
+++ RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGeneratorShould.java
@@ -2,25 +2,19 @@ package learning.branco.daniel.CoduranceKatas.romanNumerals;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class RomanNumeralsGeneratorShould {
 
-    @Test
-    void convert_1_to_I(){
-        assertEquals("I", new RomanNumeralsGenerator().convert(1));
+    @ParameterizedTest
+    @CsvSource({"1,I","2,II","3,III"})
+    void convert_arabic_numbers_to_roman_numerals(int arabicNumber, String expectedRomanNumeral){
+        assertEquals(expectedRomanNumeral, new RomanNumeralsGenerator().convert(arabicNumber));
 
     }
-
-    @Test
-    void convert_2_to_II() {
-        assertEquals("II", new RomanNumeralsGenerator().convert(2));
-    }
-
-    @Test
-    void convert_3_to_III() {
-        assertEquals("III", new RomanNumeralsGenerator().convert(3));
-    }
+     
 }
