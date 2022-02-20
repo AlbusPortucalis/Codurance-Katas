61bcee09b1e93d0b891575f5a002b953eb8f24af
Daniel Branco <daab.daab@gmail.com>
2022-02-20 11:25:58 +0100

    Added 8th test, no changes needed.

--- RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGeneratorShould.java
+++ RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGeneratorShould.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RomanNumeralsGeneratorShould {
 
     @ParameterizedTest
-    @CsvSource({"1,I","2,II","3,III", "4,IV", "5,V", "6,VI", "7,VII"})
+    @CsvSource({"1,I","2,II","3,III", "4,IV", "5,V", "6,VI", "7,VII", "8, VIII"})
     void convert_arabic_numbers_to_roman_numerals(int arabicNumber, String expectedRomanNumeral){
         assertEquals(expectedRomanNumeral, new RomanNumeralsGenerator().convert(arabicNumber));
 
