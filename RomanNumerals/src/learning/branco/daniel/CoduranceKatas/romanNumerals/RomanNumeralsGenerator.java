208e2961f8e8fd4040b7bfadffed54fc9e656552
Daniel Branco <daab.daab@gmail.com>
2022-02-20 18:42:37 +0100

    Added 24th test and code to make it green

--- RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
+++ RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
@@ -14,7 +14,8 @@ public class RomanNumeralsGenerator {
             Map.entry(10, "X"),
             Map.entry(40,"XL"),
             Map.entry(50, "L"),
-            Map.entry(90, "XC")
+            Map.entry(90, "XC"),
+            Map.entry(100,"C")
     ));
 
     public String convert(int amount){
