cdb31000f7969d99447d2fc286f226b6f1477fbe
Daniel Branco <daab.daab@gmail.com>
2022-02-20 11:23:29 +0100

    Refactored to remove excessive repetition when having I after V

--- RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
+++ RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGenerator.java
@@ -3,23 +3,26 @@ package learning.branco.daniel.CoduranceKatas.romanNumerals;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class RomanNumeralsGenerator {
-    private Map<Integer, String> romanNumerals = Map.ofEntries(
+    private TreeMap<Integer, String> romanNumerals = new TreeMap<>(Map.ofEntries(
             Map.entry(1, "I"),
             Map.entry(4, "IV"),
-            Map.entry(5, "V"),
-            Map.entry(6, "VI"),
-            Map.entry(7,"VII")
-    );
+            Map.entry(5, "V")
+    ));
 
     public String convert(int amount){
-        if(romanNumerals.containsKey(amount)){
-            return romanNumerals.get(amount);
-        }
         if(amount < 4 && amount > 0){
 
             return romanNumerals.get(1) + convert(amount - 1);
+        }
+        if(romanNumerals.containsKey(amount)){
+            return romanNumerals.get(amount);
+        }
+        if(romanNumerals.lastKey() < amount){
+            return romanNumerals.lastEntry().getValue() +
+                    convert(amount - romanNumerals.lastKey());
         }
 
 
