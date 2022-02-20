d5525b4f412ab958c7b7c8bf3445973065522a46
Daniel Branco <daab.daab@gmail.com>
2022-02-20 12:34:19 +0100

    Added 18th test, no changes need to the code

--- RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGeneratorShould.java
+++ RomanNumerals/src/learning/branco/daniel/CoduranceKatas/romanNumerals/RomanNumeralsGeneratorShould.java
@@ -11,7 +11,8 @@ public class RomanNumeralsGeneratorShould {
 
     @ParameterizedTest
     @CsvSource({"1,I","2,II","3,III", "4,IV", "5,V", "6,VI", "7,VII", "8, VIII", "9, IX",
-                "10,X", "11,XI", "12,XII", "14,XIV", "18, XVIII", "19,XIX", "20, XX", "26, XXVI"})
+                "10,X", "11,XI", "12,XII", "14,XIV", "18, XVIII", "19,XIX", "20, XX", "26, XXVI",
+                "30, XXX"})
     void convert_arabic_numbers_to_roman_numerals(int arabicNumber, String expectedRomanNumeral){
         assertEquals(expectedRomanNumeral, new RomanNumeralsGenerator().convert(arabicNumber));
 
